499d1911e69e50d269f39ffc2ea40159b07870a3
angieMutava <mutavaangela@yahoo.com>
2017-03-09 01:46:28 -0800

    modified max_min function

--- max_min.py
+++ max_min.py
@@ -3,15 +3,13 @@ def find_max_min(list_of_arguments):
     This is a function that returns the minimum and maximum value of the list provided
     if the maximum and minimum value are equal the length of the list is returned.
     """
-    max_value = list_of_arguments[0]
-    min_value = list_of_arguments[1]
-    for i in list_of_arguments:
-        if i < min_value:
-            min_value = i
-        if i > max_value:
-            max_value = i
-        if max_value == min_value:
-            return [len(list_of_arguments)]
-    return [min_value, max_value]
-
-print(find_max_min([600, 600]))
+   list_output = []
+  for values in list_of_arguments:
+    min_value = min(list_of_arguments)
+    max_value = max(list_of_arguments)
+  if max_value == min_value:
+    list_output.append(len(list_of_arguments))
+  else:
+    list_output.insert(0, min_value)
+    list_output.insert(1, max_value)
+  return list_output
